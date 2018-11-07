cc238bb1e356152bf23770e9f6b7c3a38b0ce496
chengdui <chengdui19930520@163.com>
2018-11-07 11:43:26 +0800

    fgerfe

--- src/main/java/HmgTest.java
+++ src/main/java/HmgTest.java
@@ -15,9 +15,8 @@ public class HmgTest {
 		System.out.println("巴巴爸爸巴巴巴巴爸爸");
 		System.out.println("55555555555");
 		
-		System.out.println("pppppppppppp");
-		System.out.println("tttttttttttt");
-		System.out.println("uuuuuuuuuuuuu");
+		System.out.println("p");
+		
 
 	}
 
