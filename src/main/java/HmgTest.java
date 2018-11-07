4ed2299129b1849ddda96f2ae528a400e2b6d024
Administrator <Administrator@172.29.58.92>
2018-11-07 13:55:46 +0800

    hmg-23

--- src/main/java/HmgTest.java
+++ src/main/java/HmgTest.java
@@ -29,7 +29,7 @@ public class HmgTest {
 		
 		System.out.println("uuuuuuuuuuuuu");
 		System.out.println("uuuuuuuuuuuuu");
-		System.out.println("uuuuuuuuuuuuu");
+		System.out.println("uuuuuuuuuuuuu.......");
 
 	}
 
