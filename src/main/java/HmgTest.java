811a53a20b6129e08d5ac4a648f2c820886cf00f
Administrator <Administrator@172.29.58.92>
2018-11-07 13:29:57 +0800

    del

--- src/main/java/HmgTest.java
+++ src/main/java/HmgTest.java
@@ -2,36 +2,7 @@
 public class HmgTest {
 	
 	public static void main(String[] args) {
-
 		System.out.println("hmg");
-		
-		
-				System.out.println("oooooooo");
-		
-		System.out.println("pppppppppppppppsssssss");
-		System.out.println("fdgerhvds");
-		System.out.println("pppppppppppppppsssssss");
-		System.out.println("pppppppppppppppssssss");
-		
-		System.out.println("pppppppppppppppsssssss");
-		System.out.println("pppppppppppppppsssssss");
-		
-		System.out.println("巴巴爸爸巴巴巴巴爸爸");
-		System.out.println("55555555555");
-		
-
-		System.out.println("zhaokkkkkkk");
-		
-
-		System.out.println("pppppppppppp");
-		System.out.println("tttttttttttt");
-		System.out.println("uuuuuuuuuuuuu");
-		
-		
-		System.out.println("uuuuuuuuuuuuu");
-		System.out.println("uuuuuuuuuuuuu");
-		System.out.println("uuuuuuuuuuuuu");
-
 
 	}
 
