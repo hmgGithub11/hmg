a6182b9163fa7e982288f0e05bba77158f0cc0b9
Administrator <Administrator@172.29.58.92>
2018-11-07 11:27:22 +0800

    hhhh

--- src/main/java/HmgTest.java
+++ src/main/java/HmgTest.java
@@ -14,6 +14,10 @@ public class HmgTest {
 		
 		System.out.println("巴巴爸爸巴巴巴巴爸爸");
 		System.out.println("55555555555");
+		
+		System.out.println("pppppppppppp");
+		System.out.println("tttttttttttt");
+		System.out.println("uuuuuuuuuuuuu");
 
 	}
 
